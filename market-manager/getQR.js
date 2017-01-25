9bf3527709d5fc77ea245faabb924df7d95fe7f7
Denís Graña <denisgranha@gmail.com>
2017-01-25 16:38:17 +0100

    getQR script with custom provider

---
+++ market-manager/getQR.js
@@ -0,0 +1,36 @@
+const gnosis = require("gnosisjs");
+const configObject = require("./config.js");
+const BigNumber = require('bignumber.js');
+const QRProvider = require('./uportQRProvider.js');
+const ProviderEngine = require('web3-provider-engine');
+const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
+const Web3 = require('web3');
+const engine = new ProviderEngine();
+const web3 = new Web3(engine);
+
+if (process.argv.length != 5) {
+  process.exit();
+}
+
+// Set web3
+web3.eth.defaultAccount = '0xB42E70a3c6dd57003f4bFe7B06E370d21CDA8087'
+engine.addProvider(new QRProvider());
+engine.addProvider(new RpcSubprovider({
+ rpcUrl: configObject.ethereumNodeURL,
+}));
+configObject.web3 = web3;
+engine.start();
+
+// get QR transaction string
+gnosis.contracts.marketFactory.buyShares(
+  process.argv[2], // marketHash
+  new BigNumber(process.argv[3]), // outcomeIndex
+  new BigNumber("1e18"),
+  new BigNumber("1e23"),
+  configObject,
+  process.argv[4] // marketAddress
+).then(
+  (tx) => {
+    console.log(tx.txhash);
+  }
+);
---
+++ market-manager/uportQRProvider.js
@@ -0,0 +1,49 @@
+const Subprovider = require('web3-provider-engine/subproviders/subprovider');
+
+class UportQRSubprovider extends Subprovider {
+  constructor (opts) {
+    super();
+  }
+
+  handleRequest (payload, next, end) {
+    const self = this;
+    switch (payload.method) {
+
+      case 'eth_sendTransaction':
+        let txParams = payload.params[0];
+
+        self.txParamsToUri(txParams, (err, val) => {
+          end(err, val);
+        })
+
+        return;
+
+      default:
+        next()
+        return
+
+    }
+  }
+
+  txParamsToUri (txParams, cb) {
+    let uri = 'me.uport:' + txParams.to
+    let symbol
+    if (!txParams.to) {
+      return cb(new Error('Contract creation is not supported by uportProvider'))
+    }
+    if (txParams.value) {
+      uri += '?value=' + parseInt(txParams.value, 16)
+    }
+    if (txParams.data) {
+      symbol = txParams.value ? '&' : '?'
+      uri += symbol + 'bytecode=' + txParams.data
+    }
+    if (txParams.gas) {
+      symbol = txParams.value || txParams.data ? '&' : '?'
+      uri += symbol + 'gas=' + parseInt(txParams.gas, 16)
+    }
+    cb(null, uri)
+  }
+}
+
+module.exports = UportQRSubprovider;
