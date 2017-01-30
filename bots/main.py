9753a08fde99b45c87df5aae202eda96d7552557
Giacomo <giacomo.licari@gmail.com>
2017-01-30 10:47:26 +0100

    Added a better main script

--- bots/main.py
+++ bots/main.py
@@ -1,7 +1,18 @@
+from publisher_bot.publisher_bot import PublisherBot
 from botFactory import BotFactory
+import memcache
 
 if __name__=='__main__':
+
+    memcache = memcache.Client([PublisherBot.MEMCACHE_URL], cache_cas=True)
     bot = BotFactory()
     bot.authenticate()
-    bot.start_publisher_bot()
-    bot.start_trader_bot()
+
+    if memcache.get('bot_started'):
+        # the program is already running
+        bot.start_publisher_bot()
+        print "bot already started, publish new markets"
+    else:
+        print "Getting ready... new bot started"
+        bot.start_trader_bot()
+        memcache.set('bot_started', True)
