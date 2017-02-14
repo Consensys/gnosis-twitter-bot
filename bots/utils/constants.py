e5df0c43d1e3aad6c035000b33aee5663abab75a
Giacomo <giacomo.licari@gmail.com>
2017-02-14 10:57:15 +0100

    Added UPORT url in response messages

--- bots/utils/constants.py
+++ bots/utils/constants.py
@@ -3,7 +3,8 @@ import environ
 
 MEMCACHED_URL = os.getenv('MEMCACHED_URL', '127.0.0.1:11211')
 GNOSIS_TWITTER_NAME = '@gnosismarketbot'
-GNOSIS_URL = 'https://beta.gnosis.pm/#/market/'
+GNOSIS_URL = 'https://uport.gnosis.pm/#/market/'
+UPORT_URL = 'https://www.uport.me/'
 #MARKET_MANAGER_DIR = '../market-manager/'
 #GET_MARKETS_FILE = 'getMarkets.js'
 #GET_QR_FILE = 'getQR.js'
