aaf6debc6c2ce072c36791eecc7c913145f126e9
Giacomo <giacomo.licari@gmail.com>
2017-02-20 10:17:25 +0100

    Improved trader startup script with process lookup

--- trader.sh
+++ trader.sh
@@ -1,9 +1,22 @@
-#
-# TODO
-# Check whether the Socket process is working or not
-#
-
 touch /tmp/trader.log
 chmod 777 /tmp/trader.log
+
+####################
+# Startup command
+####################
 cd bots
-python start_trader.py
+python start_trader.py&
+pid=$!
+
+while true; do
+  if  ps -p $pid > /dev/null; then
+    echo "Twitter Bot - PID $pid - is alive"
+  else
+    echo "Twitter Bot - PID $pid - is dead, restarting..."
+    python start_trader.py&
+    pid=$!
+  fi
+
+  sleep 10;
+
+done
