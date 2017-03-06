79692a2778bb5b8ac338c8a56f4884be8047051d
Giacomo <giacomo.licari@gmail.com>
2017-03-06 16:58:01 +0100

    Changed twitter_username to os.getenv

--- bots/utils/constants.py
+++ bots/utils/constants.py
@@ -2,19 +2,16 @@ import os
 import environ
 
 MEMCACHED_URL = os.getenv('MEMCACHED_URL', '127.0.0.1:11211')
-GNOSIS_TWITTER_NAME = '@gnosismarketbot'
+GNOSIS_TWITTER_NAME = os.getenv('GNOSIS_TWITTER_NAME')
+TWITTER_SCREEN_NAME= os.getenv('TWITTER_SCREEN_NAME')
 GNOSIS_URL = 'https://uport.gnosis.pm/#/market/'
 UPORT_URL = 'https://www.uport.me/'
-#MARKET_MANAGER_DIR = '../market-manager/'
-#GET_MARKETS_FILE = 'getMarkets.js'
-#GET_QR_FILE = 'getQR.js'
 
 ROOT_DIR = environ.Path(__file__) - 3
 GET_MARKETS_FILE = str(ROOT_DIR.path('market-manager/getMarkets.js'))
 GET_QR_FILE = str(ROOT_DIR.path('market-manager/getQR.js'))
 
 USER_LOCKING_TIME= (5*60) # 5 mins in seconds
-TWITTER_SCREEN_NAME='gnosismarketbot'
 
 MONGODB_HOST = os.getenv('MONGODB_HOST', 'localhost')
 MONGODB_PORT = os.getenv('MONGODB_PORT', 27017)
